af9a236ce72e809c7c8e476000472f264b65f2ef
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-11-18 22:18:21 +0330

    learning some new types of notes and numbers

--- all.cpp
+++ all.cpp
@@ -34,3 +34,9 @@ cout << myNum;
 int myNum = 15;  // myNum is 15
 myNum = 10;  // Now myNum is 10
 cout << myNum;  // Outputs 10
+
+int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false)
