b0050ba8c40e8ca4051f2438248e22b5b855dc97
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-11-22 01:30:50 +0330

    learning how to chose the type of const

--- all.cpp
+++ all.cpp
@@ -47,3 +47,6 @@ cout << x + y + z;
 
 const int myNum = 15;  // myNum will always be 15
 myNum = 10;  // error: assignment of read-only variable 'myNum'
+
+const int minutesPerHour = 60;
+const float PI = 3.14;
