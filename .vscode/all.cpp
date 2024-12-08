eae2c6c9cfd19136be6fc475ef3e0df9c475c38e
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-12-08 23:37:24 +0330

    Update all.cpp

--- .vscode/all.cpp
+++ .vscode/all.cpp
@@ -90,3 +90,10 @@ cout << "Type another number: ";
 cin >> y;
 sum = x + y;
 cout << "Sum is: " << sum;
+
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99;     // Floating point number
+double myDoubleNum = 9.98;   // Floating point number
+char myLetter = 'D';         // Character
+bool myBoolean = true;       // Boolean
+string myText = "Hello";     // String
