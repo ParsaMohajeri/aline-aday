0b1797d64c7247c34b2a47141d92216770cbb259
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-12-11 22:24:15 +0330

    Update all.cpp

--- .vscode/all.cpp
+++ .vscode/all.cpp
@@ -107,3 +107,6 @@ bool isCodingFun = true;
 bool isFishTasty = false;
 cout << isCodingFun;  // Outputs 1 (true)
 cout << isFishTasty;  // Outputs 0 (false)
+
+char myGrade = 'B';
+cout << myGrade;
