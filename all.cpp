eb4762a7ce5bcfead31422455d70df4b14655222
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-12-24 23:47:24 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -95,5 +95,9 @@ int main() {
     cout << "Cost per item: " << cost_per_item << "" << currency << "\n";
     cout << "Total cost = " << total_cost << "" << currency << "\n";
 
+    int sum1 = 100 + 50;        // 150 (100 + 50)
+    int sum2 = sum1 + 250;      // 400 (150 + 250)
+    int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
     return 0;
 }
