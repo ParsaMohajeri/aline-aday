a94d61f460ce8578bbcf353413e50b9ff96db537
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-12-31 23:42:12 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -120,5 +120,10 @@ int main() {
     string fullName = firstName + " " + lastName;
     cout << fullName;
 
+    string x = "10";
+    string y = "20";
+    string z = x + y;   // z will be 1020 (a string)
+
+
     return 0;
 }
