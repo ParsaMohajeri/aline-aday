65a5176b758a7f51c5931045e279a0ebcd2fa195
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-01-10 19:29:11 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -147,5 +147,12 @@ int main() {
 
     string txt = "It\'s alright.";
 
+    string firstName;
+    cout << "Type your first name: ";
+    cin >> firstName; // get user input from the keyboard
+    cout << "Your name is: " << firstName;
+    // Type your first name: John
+    // Your name is: John
+
     return 0;
 }
