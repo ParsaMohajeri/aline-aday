082df6b51409500317d35d7e8162a2cab0a38c3f
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-01-23 15:23:35 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -188,6 +188,7 @@ int main() {
     int x = 10;
     cout << (x == 10);  // returns 1 (true), because the value of x is equal to 10
 
+    cout << (10 == 15);  // returns 0 (false), because 10 is not equal to 15
 
     return 0;
 }
