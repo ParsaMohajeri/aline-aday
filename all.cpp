8c89078b1923827d95bedc30269b0068b6dc2341
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-01-25 23:20:31 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -194,6 +194,15 @@ int main() {
     int votingAge = 18;
     cout << (myAge >= votingAge); // returns 1 (true), meaning 25 year olds are allowed to vote!
 
+    int myAge = 25;
+    int votingAge = 18;
+    if (myAge >= votingAge) {
+    cout << "Old enough to vote!";
+    } else {
+    cout << "Not old enough to vote.";
+    }
+    // Outputs: Old enough to vote!
+
     return 0;
 }
 
