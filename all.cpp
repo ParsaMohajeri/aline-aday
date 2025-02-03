90c241fb20e728ab0ea892258af760d5506388a5
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-02-03 23:20:34 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -238,6 +238,10 @@ int main() {
     cout << "Good evening.";
     }
 
+    int time = 20;
+    string result = (time < 18) ? "Good day." : "Good evening.";
+    cout << result;
+
     return 0;
 }
 
