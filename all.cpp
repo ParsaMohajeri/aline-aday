d199af6b8478a067d8b77d7c1eeb6d2e5e28d285
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-02-05 00:14:14 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -249,6 +249,14 @@ int main() {
     cout << "Wrong code.\nThe door remains closed.\n";
     }
 
+    int myNum = 10; // Is this a positive or negative number?
+    if (myNum > 0) {
+    cout << "The value is a positive number.\n";
+    } else if (myNum < 0) {
+    cout << "The value is a negative number.\n";
+    } else {
+    cout << "The value is 0.\n";
+    }
 
     return 0;
 }
