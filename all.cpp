e39562da83062a126689ae8eb75aeafd6b30e1c5
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-02-08 23:45:19 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -299,6 +299,19 @@ int main() {
     }
     // Outputs "Thursday" (day 4)
 
+    int day = 4;
+    switch (day) {
+    case 6:
+        cout << "Today is Saturday";
+        break;
+    case 7:
+        cout << "Today is Sunday";
+        break;
+    default:
+        cout << "Looking forward to the Weekend";
+    }
+    // Outputs "Looking forward to the Weekend"
+
     return 0;
 }
 
