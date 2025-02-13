83f93a19fee2162c413cc780cc5f423218f73966
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-02-14 01:08:44 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -351,6 +351,16 @@ int main() {
     }
     cout << "Reversed numbers: " << revNumbers << "\n";
 
+    int dice = 1;
+    while (dice <= 6) {
+    if (dice < 6) {
+        cout << "No Yatzy\n";
+    } else {
+        cout << "Yatzy!\n";
+    }
+    dice = dice + 1;
+    }
+
     return 0;
 }
 
