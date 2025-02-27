924e495392c7a6b2bab4ce5fdd266cac6f1cb905
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-02-27 23:21:04 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -430,6 +430,18 @@ int main() {
     }
     }
 
+    int i = 0;
+    while (i < 10) {
+    if (i == 4) {
+        i++;
+        continue;
+    }
+    cout << i << "\n";
+    i++;
+    }
+
+    
+
     return 0;
 }
 
