01a0ccc74816d31079ea4a0a380443fb61e95bc7
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-03 00:00:12 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -448,6 +448,11 @@ int main() {
     cout << cars[0];
     // Outputs Volvo
 
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars[0] = "Opel";
+    cout << cars[0];
+    // Now outputs Opel instead of Volvo
+
 
     return 0;
 }
