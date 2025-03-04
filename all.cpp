d5c26ecedacdc029d57f2970403dc2a3abffbafb
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-04 22:58:25 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -455,10 +455,14 @@ int main() {
 
     // Create an array of strings
     string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
-
     // Loop through strings
     for (int i = 0; i < 5; i++) {
     cout << cars[i] << "\n";
+    }
+
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+    for (int i = 0; i < 5; i++) {
+    cout << i << " = " << cars[i] << "\n";
     }
 
 
