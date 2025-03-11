781c5023d714d92d0e85428c433120d5f68583b8
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-11 23:28:35 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -481,6 +481,9 @@ int main() {
     // Adding another element to the vector
     cars.push_back("Tesla");
 
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    cout << sizeof(myNumbers);
+
 
     return 0;
 }
