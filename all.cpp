60df252a3aab36219757f60d5bb53750812a7bae
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-14 00:19:25 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -493,6 +493,11 @@ int main() {
     cout << myNumbers[i] << "\n";
     }
 
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for (int i : myNumbers) {
+    cout << i << "\n";
+    }
+
     return 0;
 }
 
