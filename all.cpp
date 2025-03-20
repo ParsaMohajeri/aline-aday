39797840f42ec2523869e7b4d8c39af2a36ed560
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-20 23:36:50 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -545,6 +545,12 @@ int main() {
     }
     };
 
+    string letters[2][4] = {
+    { "A", "B", "C", "D" },
+    { "E", "F", "G", "H" }
+    };
+    cout << letters[0][2];  // Outputs "C"
+
     return 0;
 }
 
