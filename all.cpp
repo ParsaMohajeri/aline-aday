fa655a2d4761e483a7b1041d71b1399f87bae3b1
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-24 02:57:25 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -628,7 +628,12 @@ int main() {
 
 
 
+    struct {             // Structure declaration
+    int myNum;         // Member (int variable)
+    string myString;   // Member (string variable)
+    } myStructure;       // Structure variable
 
+    
 
     return 0;
 }
