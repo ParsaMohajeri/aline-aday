4eb705bfb18a4727888a849cfb90af35e111d162
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-03-25 04:34:09 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -633,7 +633,20 @@ int main() {
     string myString;   // Member (string variable)
     } myStructure;       // Structure variable
 
-    
+
+
+    struct {
+    int myNum;
+    string myString;
+    } myStructure;
+    // Assign values to members of myStructure
+    myStructure.myNum = 1;
+    myStructure.myString = "Hello World!";
+    // Print members of myStructure
+    cout << myStructure.myNum << "\n";
+    cout << myStructure.myString << "\n";
+
+
 
     return 0;
 }
