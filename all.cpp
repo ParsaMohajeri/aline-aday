0bf2b74c62d144e0db3d36455caa6ee7ec0ff0e4
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-04-07 23:39:51 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -744,6 +744,10 @@ int main() {
     string food = "Pizza";
     cout << &food; // Outputs 0x6dfed4
 
+    string food = "Pizza"; // A food variable of type string
+    cout << food;  // Outputs the value of food (Pizza)
+    cout << &food; // Outputs the memory address of food (0x6dfed4)
+
 
 
     return 0;
