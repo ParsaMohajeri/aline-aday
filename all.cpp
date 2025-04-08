c98adf277f0211e2374e028fa4feabbb56fa6335
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-04-09 00:33:34 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -755,6 +755,20 @@ int main() {
     // Dereference: Output the value of food with the pointer (Pizza)
     cout << *ptr << "\n";
 
+    string food = "Pizza";
+    string* ptr = &food;
+    // Output the value of food (Pizza)
+    cout << food << "\n";
+    // Output the memory address of food (0x6dfed4)
+    cout << &food << "\n";
+    // Access the memory address of food and output its value (Pizza)
+    cout << *ptr << "\n";
+    // Change the value of the pointer
+    *ptr = "Hamburger";
+    // Output the new value of the pointer (Hamburger)
+    cout << *ptr << "\n";
+    // Output the new value of the food variable (Hamburger)
+    cout << food << "\n";
 
 
 
