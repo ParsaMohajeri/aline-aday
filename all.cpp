34906bd8f7e29f57abfeee397ccf7cf74388e40f
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-04-18 00:04:43 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -815,7 +815,6 @@ int main() {
         void myFunction(string country = "Norway") {
             cout << country << "\n";
         }
-
         int main() {
             myFunction("Sweden");
             myFunction("India");
@@ -823,6 +822,21 @@ int main() {
             myFunction("USA");
             return 0;
         }
+
+    void myFunction(string fname, int age) {
+    cout << fname << " Refsnes. " << age << " years old. \n";
+    }
+
+    int main() {
+    myFunction("Liam", 3);
+    myFunction("Jenny", 14);
+    myFunction("Anja", 30);
+    return 0;
+    }
+ 
+    // Liam Refsnes. 3 years old.
+    // Jenny Refsnes. 14 years old.
+    // Anja Refsnes. 30 years old.
 
 
     return 0;
