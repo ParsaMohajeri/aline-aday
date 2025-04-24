1cae5f289357a441d0bafe149f1e2fdd1f25a76b
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-04-25 02:25:20 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -898,6 +898,16 @@ int main() {
   return 0;
 }
 
+void modifyStr(string &str) {
+  str += " World!";
+}
+
+int main() {
+  string greeting = "Hello";
+  modifyStr(greeting);
+  cout << greeting;
+  return 0;
+}
 
     return 0;
 }
