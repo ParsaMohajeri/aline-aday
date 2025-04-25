dcef7455d78570c10fe9cf53ebb558fa46f4e87b
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-04-26 00:43:48 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -909,6 +909,18 @@ int main() {
   return 0;
 }
 
+void myFunction(int myNumbers[5]) {
+  for (int i = 0; i < 5; i++) {
+    cout << myNumbers[i] << "\n";
+  }
+}
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  myFunction(myNumbers);
+  return 0;
+}
+
     return 0;
 }
 
