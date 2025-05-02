831ba4d5c52ed1ead83ee16c51b5fc8d86b7bdf8
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-05-02 22:46:15 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -4,9 +4,10 @@ using namespace std;  // Apply this for using cout directly without std::
 int main() {
     // Basic output examples
     cout << "Hello World!" << endl;
-    cout << "Hello World!" << endl;
-    cout << "Hello World!" << endl;
-
     return 0;
 }
 
+int main1() {
+    std::cout << "Hello World!";
+    return 0;
+  }
