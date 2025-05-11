bbc64ce3e7ba8f4ce8e3d0d455d433c4fbf0136e
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-05-11 23:47:10 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -15,6 +15,10 @@ int main() {
     cout << "I am learning C++";
     int myNum = 15;
     cout << myNum;
+    int myNum = 15;  // myNum is 15
+    myNum = 10;  // Now myNum is 10
+    cout << myNum;  // Outputs 10
+
     return 0;
 }
 
