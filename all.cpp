dcab02b4f13c4098a9912bed2cc321da0cc6d5bd
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-05-14 23:19:58 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -18,6 +18,8 @@ int main() {
     int myNum = 15;  // myNum is 15
     myNum = 10;  // Now myNum is 10
     cout << myNum;  // Outputs 10
+    int x = 5, y = 6, z = 50;
+    cout << x + y + z;
 
     return 0;
 }
