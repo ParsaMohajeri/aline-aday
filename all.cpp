f238ff98cb9dbdaa8cec00bf279920dada68ee22
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-05-25 22:31:02 +0330

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -50,6 +50,19 @@ int main() {
     cout << "Width is: " << width << "\n";
     cout << "Area of the rectangle is: " << area << "\n";
 
+    int x; 
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "Your number is: " << x; // Display the input value
+    int x, y;
+    int sum;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum is: " << sum;
+
     return 0;
 }
 
